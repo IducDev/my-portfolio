9ce02c393c7a01e3690c0392ff3912ff3dd62b7c
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-08-29 23:00:54 -0400

    working yet

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -22,8 +22,10 @@ const Services = () => {
     <div id='services' className='w-full  bg-bluePrimary p-5 '>
         <h2 className='text-skyClear text-[1.5rem] md:text-[3rem] mb-8'>Experience</h2>
         {/*Timeline*/}
-        <ol className="items-center sm:flex">
-            <li className="relative mb-6 sm:mb-0">
+        <ol className="grid grid-cols-1 md:grid-cols-3">
+            {/*por cada fila solo pueden ir tres columnas de experiencias*/}
+            
+            <li className="relative mb-6 sm:mb-0 col-span-1">
                 <div className="flex items-center">
                     <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
                         <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -42,99 +44,59 @@ const Services = () => {
                         </a>
                     </h3>
                     <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 mt-3">Released on January, 2023</time>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Perform tasks such as layout designer, endpoint consumption, centralized and persistent state management, project deployment.
+                    </p>
                 </div>
             </li>
-            
-            
-            {/*
-            <li className="relative mb-6 sm:mb-0">
-                <div className="flex items-center">
-                    <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                        </svg>
-                    </div>
-                    <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                </div>
-                <div className="mt-3 sm:pr-8">
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        <span className="mb-5">
-                            Frontend Developer  - 
-                        </span>
-                        <img src={nocountrylogo} alt="no-country" className="w-[7rem] md:w-[10rem] mt-3" />
-                    </h3>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 mt-3">Released on January, 2023</time>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                </div>
-            </li>
-            <li className="relative mb-6 sm:mb-0">
-                <div className="flex items-center">
-                    <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                        </svg>
-                    </div>
-                    <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                </div>
-                <div className="mt-3 sm:pr-8">
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                        <span className="mb-5">
-                            Frontend Developer  - 
-                        </span>
-                        <img src={nocountrylogo} alt="no-country" className="w-[7rem] md:w-[10rem] mt-3" />
-                    </h3>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 mt-3">Released on January, 2023</time>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                </div>
-            </li>
-            */}
         </ol>
+
+
         <h2 className='text-skyClear text-[1.5rem] md:text-[3rem] mb-8 mt-8'>Skills</h2>
         <div className="grid grid-cols-5 gap-4 w-full lg:w-[700px] mx-auto mt-5">
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={javascript}  alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={html} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={css} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={tailwind} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={typeScript} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={firebaseLogo} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={gitLogo} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={githubLogo} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={linux} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={express} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={grapfQL} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={nest} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={mongoDB} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={redux} alt="img"/>
                 </div>
-                <div>
+                <div className="transition-transform duration-300 transform-gpu hover:scale-50">
                     <img className="h-auto max-w-full rounded-lg" src={node} alt="img"/>
                 </div>
         </div>
