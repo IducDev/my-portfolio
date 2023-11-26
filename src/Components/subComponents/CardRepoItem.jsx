ab526bbbaee2be4cda9c567223ed29d3da4a2d43
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-11-26 09:59:24 -0400

    working yet

--- src/Components/subComponents/CardRepoItem.jsx
+++ src/Components/subComponents/CardRepoItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import repository from "../../assets/carpeta.png";
+import linkToSite from "../../assets/cadena.png";
 
 const CardRepoItem = ({repo}) => {
 
@@ -8,27 +10,23 @@ const CardRepoItem = ({repo}) => {
         <div className="h-[100%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             
             <div className="p-5 h-[100%] flex flex-col justify-between">
-                <a href={repo.html_url} target='_blank' rel='noreferrer'>
+                <div>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         {repo.name}
                     </h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
 
-                </a>
+                </div>
                 
                 <div className='w-[100%] flex justify-between align-bottom'>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-bluePrimary rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href={repo.html_url} target='_blank' rel='noreferrer' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-bluePrimary rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Repository
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor"  d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
+                        <img src={repository} alt='repository' className='w-[1.5rem] ml-2' />
                     </a>
 
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href={repo.homepage} target='_blank' rel='noreferrer' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Link
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor"  d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
+                        <img src={linkToSite} alt='link' className='w-[1.5rem] ml-2' />
                     </a>
                 </div>
             </div>
