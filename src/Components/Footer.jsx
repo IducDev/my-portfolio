ed34e9578d95a864a62ee3666feddde44ef64062
iducDev <urdanetacuarteisaacdavid@gmail.com>
2024-06-02 00:38:34 -0400

    working yet

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="bg-blueSecondary shadow ">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://github.com/IducSoft" target='_blank' rel='noreferrer' className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <a href="https://github.com/IducDev" target='_blank' rel='noreferrer' className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src={Atomo} className="h-8" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Isaac Urdaneta</span>
                     </a>
