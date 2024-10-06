bd5f5aa91db13b95c86e2d009aaba986e74af853
iducDev <urdanetacuarteisaacdavid@gmail.com>
2024-10-06 10:38:01 -0400

    estoy actualizando mi Portafolio

--- app/ui/Services.tsx
+++ app/ui/Services.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import nocountrylogo from "../../public/nocountrylogo.png"
+import LogoSpotter from "../../public/Frame 145.png"
 import javascript from "../../public/js.png";
 import html from "../../public/html-5.png";
 import css from "../../public/css-3.png";
@@ -48,6 +49,36 @@ const Services = () => {
                     <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 mt-3">From January 2023 to January 2024</time>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">
                         Play the role of frontend developer with the following technologies: Typescript, Nextjs, TailwindCss, Redux, Zustand. I collaborated closely with the UI/UX team and the backend team, you could say I was a kind of bridge for both worlds.
+                    </p>
+                </div>
+            </li>
+
+            <li className="relative mb-6 sm:mb-0 col-span-1">
+                <div className="flex items-center">
+                    <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </div>
+                    <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                </div>
+                <div className="mt-3 sm:pr-8">
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        <span className="mb-5">
+                            Frontend Developer  - Mobile Developer
+                        </span>
+                        <a href='https://www.nocountry.tech' rel='noreferrer' target="_blank" className='bg-white'>
+                            <Image src={LogoSpotter} alt="no-country" className="w-[7rem] md:w-[10rem] mt-3"  />
+                        </a>
+                        <p>SpotterGym</p>
+                    </h3>
+                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 mt-3">From February 2024 - Present</time>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Designing the screens, 
+                        components and user experiences in the app,
+                        Layout views and components to fit all screen widths, 
+                        Integration and communication with the backend team to consume endpoints, 
+                        communication with the server.
                     </p>
                 </div>
             </li>
