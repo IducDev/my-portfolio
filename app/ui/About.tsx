1268774d44e5f0bebab6ea56a83198ea9185da4b
Isaac Urdaneta <78765905+IducDev@users.noreply.github.com>
2025-01-03 10:55:41 -0400

    Update About.tsx

--- app/ui/About.tsx
+++ app/ui/About.tsx
@@ -26,7 +26,14 @@ const About = () => {
             repeat={Infinity}/>
           </div>
           <p className='text-white mt-4 '>
-            I am Isaac Urdaneta, a passionate Frontend developer with three years of experience in the field. My educational background and professional experience have allowed me to acquire strong skills in TypeScript and JavaScript, allowing me to create interactive and efficient web applications using React and Next.js. My passion for web development led me to explore beyond traditional front-end technologies, and I am currently expanding my knowledge in Node.js and the Flutter ecosystem, seeking to integrate these new tools into my projects to offer more complete solutions and robust.
+            I am Isaac Urdaneta, a passionate Frontend Developer with three years of
+            experience in the industry. My educational background and professional
+            journey have equipped me with strong skills in TypeScript and JavaScript,
+            enabling me to develop interactive and efficient web applications using React
+            and Next.js. Driven by my enthusiasm for web development, I have expanded
+            my expertise beyond traditional front-end technologies. I am currently
+            enhancing my skills in the Flutter ecosystem, aiming to integrate these tools
+            into my projects to deliver comprehensive and robust solutions.
           </p>
         
           <button type="button" className="mt-[1rem] py-5 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ background: 'linear-gradient(to right, #12152c, #123e73, #1fa7d4, #20dad9, #93e9e7)' }}>
