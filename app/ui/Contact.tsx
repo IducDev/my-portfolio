ddb59df1d34c03412bbd7361cad44d9615f9f0be
iducDev <urdanetacuarteisaacdavid@gmail.com>
2025-01-03 11:35:51 -0400

    añadido boton de youtube

--- app/ui/Contact.tsx
+++ app/ui/Contact.tsx
@@ -2,14 +2,15 @@
 import linkedin from "../../public/linkedin.png";
 import gmail from "../../public/gmail.png";
 import github from "../../public/github.png";
+import youtube from "../../public/youtube.png";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "./Footer";
 const Contact = () => {
   return (
     <div id='contact' className='flex flex-col justify-between contact w-full h-screen bg-bluePrimary'>
-          <h5 className='text-skyClear text-[1.5rem] md:text-[3rem] px-5'>Contact me</h5>
-          <div className='grid grid-cols-1 gap-4 sm:grid-cols-3 items-center' id='icon-container'>
+          <h5 className='text-skyClear text-[1.5rem] md:text-[3rem] px-5'>Social Media</h5>
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-4 items-center' id='icon-container'>
             <div className="flex justify-center items-center ">
               <Link href="https://www.linkedin.com/in/isaac-urdaneta" target="_blank" rel="noreferrer" >
               <Image className="w-[100px] rounded-lg" src={linkedin}  alt="img"/>
@@ -18,6 +19,11 @@ const Contact = () => {
             <div className="flex justify-center items-center">
               <Link href="mailto:urdanetacuarteisaacdavid@gmail.com" target="_blank" rel="noreferrer" >
                 <Image className="w-[100px] rounded-lg" src={gmail}  alt="img"/>
+              </Link>
+            </div>
+            <div className="flex justify-center items-center">
+              <Link href="https://www.youtube.com/@iducDev" target="_blank" rel="noreferrer" >
+                <Image className="w-[100px] rounded-lg" src={youtube}  alt="img"/>
               </Link>
             </div>
             <div className="flex justify-center items-center ">
