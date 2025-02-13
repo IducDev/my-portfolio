229d67454c9ad1cbae277f1d17d6def0576e62b7
Isaac Urdaneta <78765905+IducDev@users.noreply.github.com>
2025-02-13 10:43:00 -0400

    Update About.tsx

--- app/ui/About.tsx
+++ app/ui/About.tsx
@@ -37,7 +37,7 @@ const About = () => {
           </p>
         
           <button type="button" className="mt-[1rem] py-5 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ background: 'linear-gradient(to right, #12152c, #123e73, #1fa7d4, #20dad9, #93e9e7)' }}>
-            <a className='text-[1.5rem]' target='_blank' rel='noreferrer'  href='https://drive.google.com/file/d/1ifgnWPkeENhMTvKM3fj1PdX0WjVTxI-9/view' >
+            <a className='text-[1.5rem]' target='_blank' rel='noreferrer'  href='https://drive.google.com/file/d/1MlDE3Oid7nTjUW8WsffUULEbnFdg2Hma/view?usp=sharing' >
               Download CV
             </a>
           </button>
