b72fdcc3d6fcac2d5732b0be1d9517013ab9da2a
iducDev <urdanetacuarteisaacdavid@gmail.com>
2025-03-16 11:10:45 -0400

    avances

--- app/ui/About.tsx
+++ app/ui/About.tsx
@@ -27,14 +27,9 @@ const About = () => {
             repeat={Infinity}/>
           </div>
           <p className='text-white mt-4 '>
-            I am Isaac Urdaneta, a passionate Frontend Developer with three years of
-            experience in the industry. My educational background and professional
-            journey have equipped me with strong skills in TypeScript and JavaScript,
-            enabling me to develop interactive and efficient web applications using React
-            and Next.js. Driven by my enthusiasm for web development, I have expanded
-            my expertise beyond traditional front-end technologies. I am currently
-            enhancing my skills in the Flutter ecosystem, aiming to integrate these tools
-            into my projects to deliver comprehensive and robust solutions.
+            With a solid foundation in modern web and mobile development, I bring hands-on experience in: TypeScript & Next.js: 
+            Building scalable, high-performance web applications with TypeScript and Next.js, leveraging server-side rendering (SSR) and static site generation (SSG) for optimal user experiences. Dart & Flutter: Developing cross-platform mobile applications with Flutter, creating beautiful and responsive UIs that work seamlessly on Android. PostgreSQL & Supabase: Designing and managing robust databases with PostgreSQL, and utilizing Supabase for backend development, including authentication, real-time data, and serverless functions. 
+            I am passionate about creating efficient, user-friendly solutions that solve real-world problems. Let's build something amazing together!
           </p>
         
           <button type="button" className="mt-[1rem] py-5 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ background: 'linear-gradient(to right, #12152c, #123e73, #1fa7d4, #20dad9, #93e9e7)' }}>
